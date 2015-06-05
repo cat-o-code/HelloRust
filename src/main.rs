4c5b26a1c6864b25d181fde57a84a467b98f3b34
MaxPaw <m4xpaw@gmail.com>
2015-06-05 15:22:33 +0300

    1
    1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
 use std::io;
-use std::io::prelude::*;
+
+fn readoneline(){
+	let mut reader = io::stdin();
+	let mut input_text = String::new();
+	reader.read_line(&mut input_text).ok().expect("failed to read line");
+	let input_opt: Option<i32> = input_text.trim().parse::<i32>().ok();
+	let input_int = match input_opt {
+			Some(input_int) => {
+				input_int;
+			},
+			None			=> {
+				println!("please input a number");
+				return;
+			}
+	};	
+	return;
+}
 
 fn main() {
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        println!("{}", line.unwrap());
-    }
+
+
+
+	println!("Please input 1st number");
+	
+	let mut number = readoneline();
+
+	println!("{:?}", number);
 }
