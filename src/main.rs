2e589f60af7112622ad5cc97c84eaece4ff92c5d
MaxPaw <m4xpaw@gmail.com>
2015-06-08 18:03:34 +0300

    Simplified usage, error message avoiding
    Simplified main fn
    Warped all stuff to readnumber fn
    Got rid of error messages

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,40 @@
 use std::io;
+use std::thread::sleep_ms;
 
+fn pressanykey() {
+	sleep_ms(3000);
+} // will get to this later, poor windows
 
-
-fn readoneline() -> (i64,bool){
+fn readnumber(message: &'static str) -> (i64,bool){
 	let mut reader = io::stdin();
-	let mut input_text = String::new();
-
-	reader.read_line(&mut input_text).ok().expect("failed to read line");
-	let input_opt: Option<i64> = input_text.trim().parse::<i64>().ok();
-	let input_int = match input_opt {
+	println!("{}", message);
+	loop {
+		let mut input_text = String::new();
+		reader.read_line(&mut input_text).ok();
+		let input_opt: Option<i64> = input_text.trim().parse::<i64>().ok();
+		let input_int = match input_opt {
 			Some(input_int) => {
 				return (input_int,true);
 			},
 			None			=> {
-				println!("The input was not a number");
-				return (0,false);
+				println!("{}, again", message);
+				continue;
 			}
-	};	
+		};
+	}	
 	return (0,false);
 }
 
 fn main() {
 
-	let mut number1; 
-	let mut number2;
 
-	loop {	
-		println!("Please input 1st number");
-		number1 = readoneline();
-		if number1.1 {
-			break;
-		} else {
-			continue;
-		}
-	}
-	loop {	
-		println!("Please input 2st number");
-		number2 = readoneline();
-		if number2.1 {
-			break;
-		} else {
-			continue;
-		}
-	}
+	let mut number1 = readnumber("Please input 1st number");
+	let mut number2 = readnumber("Please input 2nd number");
+
 
 	println!("SUM is {0:?}", number1.0 + number2.0);
 	println!("DIF is {0:?}", number1.0 - number2.0);
 	println!("PRO is {0:?}", number1.0 * number2.0);
 	println!("DIV is {0:?}", number1.0 / number2.0);
-
+	pressanykey();
 }
