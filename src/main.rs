a25192ca45f5a7109b8f39f911a3e672af95cf60
MaxPaw <m4xpaw@gmail.com>
2015-06-08 11:24:30 +0300

    Calculator created
    It is done for the 1st time

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,52 @@
 use std::io;
 
-fn readoneline(){
+
+
+fn readoneline() -> (i64,bool){
 	let mut reader = io::stdin();
 	let mut input_text = String::new();
+
 	reader.read_line(&mut input_text).ok().expect("failed to read line");
-	let input_opt: Option<i32> = input_text.trim().parse::<i32>().ok();
+	let input_opt: Option<i64> = input_text.trim().parse::<i64>().ok();
 	let input_int = match input_opt {
 			Some(input_int) => {
-				input_int;
+				return (input_int,true);
 			},
 			None			=> {
-				println!("please input a number");
-				return;
+				println!("The input was not a number");
+				return (0,false);
 			}
 	};	
-	return;
+	return (0,false);
 }
 
 fn main() {
 
+	let mut number1; 
+	let mut number2;
 
+	loop {	
+		println!("Please input 1st number");
+		number1 = readoneline();
+		if number1.1 {
+			break;
+		} else {
+			continue;
+		}
+	}
+	loop {	
+		println!("Please input 2st number");
+		number2 = readoneline();
+		if number2.1 {
+			break;
+		} else {
+			continue;
+		}
+	}
 
-	println!("Please input 1st number");
-	
-	let mut number = readoneline();
+	println!("SUM is {0:?}", number1.0 + number2.0);
+	println!("DIF is {0:?}", number1.0 - number2.0);
+	println!("PRO is {0:?}", number1.0 * number2.0);
+	println!("DIV is {0:?}", number1.0 / number2.0);
 
-	println!("{:?}", number);
 }
